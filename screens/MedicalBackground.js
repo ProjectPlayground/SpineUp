7f99217063cb2a2541cd12ac43676106d812d58a
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-02 13:57:47 +0000

    medical background page complete

--- screens/MedicalBackground.js
+++ screens/MedicalBackground.js
@@ -1,19 +1,76 @@
 /* @flow weak */
 
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
 
-const MedicalBackground = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text>I'm MedicalBackground</Text>
-    <Button title="Confirm" onPress={() => navigation.pop()} />
-  </View>
-);
+import { Block, Text, Button, Input } from '../components';
+import { theme } from '../constants';
+
+const MedicalBackground = ({ navigation }) => {
+  const [message, setMessage] = React.useState('');
+  return (
+    <Block
+      style={styles.container}
+      color="white"
+      padding={[theme.sizes.padding * 1.2, theme.sizes.padding * 2]}
+    >
+      <Text h1 bold center>
+        Tell us about your medical background
+      </Text>
+      <Block style={styles.card} flex={0.6}>
+        <Block style={styles.button} flex={0.2}>
+          <Input
+            label="message"
+            style={styles.input}
+            defaultValue={message}
+            onChangeText={(userMessage) => setMessage(userMessage)}
+            autoCapitalize="none"
+          />
+        </Block>
+      </Block>
+      <Block flex={0.2}>
+        <Button
+          gradient
+          style={styles.confirmButton}
+          onPress={() => navigation.pop()}
+        >
+          <Text center white>
+            Confirm
+          </Text>
+        </Button>
+      </Block>
+    </Block>
+  );
+};
 
 export default MedicalBackground;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  card: {
+    marginTop: theme.sizes.padding * 3,
+    marginBottom: theme.sizes.padding * 2,
+    paddingTop: theme.sizes.padding / 2,
+    paddingLeft: theme.sizes.padding / 2,
+    borderRadius: theme.sizes.radius + 4,
+    height: 100,
+    width: '100%',
+    elevation: 10,
+    shadowColor: '#0000ff',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 5,
+    //justifyContent: 'center',
+    //alignItems: 'center',
+    backgroundColor: theme.colors.white,
+  },
+  confirmButton: {
+    paddingLeft: theme.sizes.padding * 1.2,
+    paddingRight: theme.sizes.padding * 1.2,
+  },
+  input: {
+    borderWidth: 0,
   },
 });
--- screens/MedicalImages.js
+++ screens/MedicalImages.js
@@ -1,13 +1,37 @@
 /* @flow weak */
 
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import { Block, Text, Button } from '../components';
+import { theme } from '../constants';
 
 const MedicalImages = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text>I'm MedicalImages</Text>
-    <Button title="Confirm" onPress={() => navigation.pop()} />
-  </View>
+  <Block
+    style={styles.container}
+    color="white"
+    padding={[theme.sizes.padding, theme.sizes.padding * 1.2]}
+  >
+    <Text h1 bold center>
+      Upload Images
+    </Text>
+    <Block style={styles.card} flex={0.6}>
+      <Block style={styles.button} flex={0.2}>
+        <Button onPress={() => {}}>
+          <Text size={18} color="#40C4FF">
+            Choose Images
+          </Text>
+        </Button>
+      </Block>
+    </Block>
+    <Block flex={0.2} >
+      <Button gradient style={styles.confirmButton}>
+        <Text center white>
+          Confirm
+        </Text>
+      </Button>
+    </Block>
+  </Block>
 );
 
 export default MedicalImages;
@@ -15,5 +39,32 @@ export default MedicalImages;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  card: {
+    marginTop: theme.sizes.padding * 3,
+    marginBottom: theme.sizes.padding * 2,
+    borderRadius: theme.sizes.radius + 4,
+    height: 100,
+    width: '100%',
+    elevation: 10,
+    shadowColor: '#0000ff',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.white,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#40C4FF',
+    borderRadius: 30,
+    width: 180,
+  },
+  confirmButton: {
+    paddingLeft: theme.sizes.padding * 1.2,
+    paddingRight: theme.sizes.padding * 1.2,
   },
 });
